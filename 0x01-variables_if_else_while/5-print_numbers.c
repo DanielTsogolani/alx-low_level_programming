13e53f7d099f57a025d5f6385f9feb7a073ac986
DanielTsogolani <tsogolanidaniel23@gmail.com>
2022-10-15 08:33:39 +0000

    Add 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int digit = 0;
+
+	for (; digit != 10; digit++)
+		printf("%d", digit);
+	printf("\n");
+	return (0);
+}
