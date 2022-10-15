04fae12b5631ad85040e3df9e49a36625920301b
DanielTsogolani <tsogolanidaniel23@gmail.com>
2022-10-15 08:42:07 +0000

    Add 6-print_numbers.c

---
+++ 0x01-variables_if_else_while/6-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num = 48;
+
+	for (; num <= 57; num++)
+		putchar(num);
+	putchar('\n');
+	return (0);
+}
