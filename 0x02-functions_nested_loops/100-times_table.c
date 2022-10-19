b21c97b357ae94c8c39633641337623f97647dd3
DanielTsogolani <tsogolanidaniel23@gmail.com>
2022-10-19 10:04:29 +0000

    Add tasks 101 to 104

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,10 +7,11 @@
       */
 void print_times_table(int n)
 {
-	        print_times_table(2);
+	       /* print_times_table(2); */
 
-			int num, mult, prod;
-
+			int num;
+			int mult;
+			int prod;
 				if (n >= 0 && n <= 15)
 						{
 									for (num = 0; num <= n; num++)
