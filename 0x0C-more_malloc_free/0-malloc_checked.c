9cc405006be3e74990f10daa27a5de4bf902c1c1
DanielTsogolani <tsogolanidaniel23@gmail.com>
2022-11-16 06:27:49 +0000

    Refactor

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -18,24 +18,4 @@ void *malloc_checked(unsigned int b)
 		return (NULL);
 	}
 	return (p);
-#include "main.h"
-#include <stdlib.h>
-
-/**
- *malloc_checked - allocate memory.
- *@b: size of the memory to be allocated.
- *Return: pointer;
- **/
-
-void *malloc_checked(unsigned int b)
-{
-	char *p;
-
-	p = malloc(b);
-	if (p == NULL)
-	{
-		exit(98);
-		return (NULL);
-	}
-	return (p);
 }
